d5d556e4ef02e02ccfd73c125081af0b1e3bf4c2
Administrator <BUTTOWSKIKIK33@GMAIL.COM>
2024-02-05 12:44:39 +0530

    Initial commit

---
+++ src/main/java/com/example/Projectk/EventBuzzApplication.java
@@ -0,0 +1,12 @@
+package com.example.Projectk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBuzzApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/Projectk/EventBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projectk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
